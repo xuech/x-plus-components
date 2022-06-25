bd170245d3ec37912ce9041a50ffe7c77b25b5fc
xuech_love_ <xuech_love@126.com>
2022-06-25 11:37:45 +0800

    scss

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,9 @@
   <router-view></router-view>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 160px;
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
