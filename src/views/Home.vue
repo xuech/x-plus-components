a5182455b96e3795a845815bc082d96084629e42
xuech_love_ <xuech_love@126.com>
2022-06-25 11:35:22 +0800

    搭建vite项目并配置路由和element-plus

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        home
+         <el-button type="primary" round>Primary</el-button>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
