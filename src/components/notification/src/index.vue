d636f5559129e9d1aacf88c29e398ba82ca4e995
xuech_love_ <xuech_love@126.com>
2022-06-27 21:56:13 +0800

    通知组件

---
+++ src/components/notification/src/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-badge :value="value" :max="max" :is-dot="isDot">
+  <component :is="`el-icon-${toLine(icon)}`"></component>
+
+  </el-badge>
+</template>
+
+<script lang="ts" setup>
+import {toLine} from '../../../utils';
+const props = defineProps({
+  icon: {
+    type: String,
+    default: 'Bell',
+  },
+  vaule: {
+    type: [String,Number],
+    default: '',
+  },
+  max: {
+    type: Number,
+    default: 0,
+  },
+  isDot: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/notification/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="flex">
+      <m-notification :value="50"></m-notification>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+}
+</style>
