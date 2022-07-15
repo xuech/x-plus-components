377a13bb9753abd26effb5f94150eabe7cf57704
xuech_love_ <xuech_love@126.com>
2022-07-15 13:13:21 +0800

    #feat 更新图标信息

--- packages/index.ts
+++ packages/index.ts
@@ -8,6 +8,8 @@ import list from './list'
 import menu from './menu'
 import './style/base.scss'
 import './style/ui.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { toLine } from './utils'
 
 const components = [
   chooseArea,
@@ -20,6 +22,9 @@ const components = [
 ]
 export default {
   install(app: App) {
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(`el-icon-${toLine(key)}`, component)
+    }
     components.map((item) => {
       app.use(item)
     })
--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,14 @@ import 'element-plus/dist/index.css'
 import { toLine } from './utils'
 // import mUI from '@/components'
 // 全量引入
-import mUI from '../lib/index.es.js'
+// import mUI from '../lib/index.es.js'
+import mUI from 'x-plus-components'
+import 'x-plus-components/style.css'
+
 //单独引入
 // import chooseIcon from '../lib/chooseIcon/index.es.js'
 // import '../lib/chooseIcon/style.css'
-import '../lib/style.css'
+// import '../lib/style.css'
 
 const app = createApp(App)
 // 全局注册element图标 ？是否合理
